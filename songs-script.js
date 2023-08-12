1fb723da124ac6902c60bba902cd3412120bb6ce
adyamoha <amohanty533@gmail.com>
2023-08-11 21:09:28 -0400

    debug shake 2

--- songs-script.js
+++ songs-script.js
@@ -1,18 +1,25 @@
 // songs-script.js
-document.getElementById("password-input").addEventListener("keydown", function(event) {
-    console.log("Key pressed:", event.key);
-    if (event.key === "Enter") {
-        const inputPassword = this.value;
-        const correctPassword = "canthurry";
 
-        if (inputPassword === correctPassword) {
-            document.getElementById("password-input").classList.remove("shake");
-            document.getElementById("content").style.opacity = 1;
-        } else {
-            document.getElementById("password-input").classList.add("shake");
-            setTimeout(() => {
+document.addEventListener("DOMContentLoaded", function() {
+   
+    document.getElementById("password-input").addEventListener("keydown", function(event) {
+        console.log("Key pressed:", event.key);
+        if (event.key === "Enter") {
+            const inputPassword = this.value;
+            const correctPassword = "canthurry";
+    
+            if (inputPassword === correctPassword) {
                 document.getElementById("password-input").classList.remove("shake");
-            }, 500); // Adjust the delay as needed
+                document.getElementById("content").style.opacity = 1;
+            } else {
+                document.getElementById("password-input").classList.add("shake");
+                setTimeout(() => {
+                    document.getElementById("password-input").classList.remove("shake");
+                }, 500); // Adjust the delay as needed
+            }
         }
-    }
+    });
+
 });
+
+
